a95e1e474e1613e1cfc3818c26be3bc062246f3a
AndreyDenisov0197 <denisov0197@yandex.ru>
2024-02-24 18:13:01 +0300

    fix: убрать импорты

--- src/main/java/ru/practicum/shareit/exception/ExeptionHandler.java
+++ src/main/java/ru/practicum/shareit/exception/ExeptionHandler.java
@@ -3,17 +3,11 @@ package ru.practicum.shareit.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.validation.ConstraintViolationException;
-import javax.validation.ValidationException;
-import java.sql.SQLException;
-import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
--- src/main/java/ru/practicum/shareit/exception/NotAvailableItemException.java
+++ src/main/java/ru/practicum/shareit/exception/NotAvailableItemException.java
@@ -1,6 +1,6 @@
 package ru.practicum.shareit.exception;
 
-public class NotAvailableItemException extends RuntimeException{
+public class NotAvailableItemException extends RuntimeException {
     public NotAvailableItemException(String message) {
         super(message);
     }
