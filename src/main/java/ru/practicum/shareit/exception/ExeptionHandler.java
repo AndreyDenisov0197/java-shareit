c892845dd30e27917fd7473a5daa271e8168b063
AndreyDenisov0197 <denisov0197@yandex.ru>
2024-02-25 13:46:35 +0300

    fix: добавить обработку новых исключений валидации

--- src/main/java/ru/practicum/shareit/exception/ExeptionHandler.java
+++ src/main/java/ru/practicum/shareit/exception/ExeptionHandler.java
@@ -2,12 +2,13 @@ package ru.practicum.shareit.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.validation.ConstraintViolationException;
+import javax.validation.ValidationException;
 import java.util.Map;
 
 @Slf4j
@@ -15,9 +16,9 @@ import java.util.Map;
 public class ExeptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<String> notFoundHandle(Exception e) {
+    public ErorrResponse<String> notFoundHandle(Exception e) {
         log.error("{} : {}", e.getClass().getSimpleName(), e.getMessage());
-        return new ResponseEntity<>(e.getClass().getSimpleName() + " : " + e.getMessage(),
+        return new ErorrResponse<>(e.getClass().getSimpleName() + " : " + e.getMessage(),
                 HttpStatus.NOT_FOUND);
     }
 
@@ -28,11 +29,20 @@ public class ExeptionHandler {
         return Map.of("error", e.getConstraintViolations().stream().findFirst().get().getMessage());
     }
 
-    @ExceptionHandler(NotAvailableItemException.class)
-    public ResponseEntity<String> exceptionHandle(Exception e) {
+    @ExceptionHandler({NotAvailableItemException.class,
+            MethodArgumentNotValidException.class,
+            ValidationException.class})
+    public ErorrResponse<String> exceptionHandle(Exception e) {
         log.error("{} : {}", e.getClass().getSimpleName(), e.getMessage());
-        return new ResponseEntity<>(e.getClass().getSimpleName() + " : " + e.getMessage(),
+        return new ErorrResponse<>(e.getClass().getSimpleName() + " : " + e.getMessage(),
                 HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Throwable.class)
+    public ErorrResponse<String> throwableHandle(Throwable e) {
+        log.error("{} : {}", e.getClass().getSimpleName(), e.getMessage());
+        return new ErorrResponse<>(e.getClass().getSimpleName() + " : " + e.getMessage(),
+                HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
