0e7a8ab4a9c12cbbd6d31ef11d18eeb06096524a
AndreyDenisov0197 <denisov0197@yandex.ru>
2024-03-06 16:21:07 +0300

    feat: добавить тесты

--- src/main/java/ru/practicum/shareit/exception/NotAvailableItemException.java
+++ src/main/java/ru/practicum/shareit/exception/NotAvailableItemException.java
@@ -1,7 +1,13 @@
 package ru.practicum.shareit.exception;
 
+import lombok.Getter;
+
+@Getter
 public class NotAvailableItemException extends RuntimeException {
+    private final String message;
+
     public NotAvailableItemException(String message) {
         super(message);
+        this.message = message;
     }
 }
